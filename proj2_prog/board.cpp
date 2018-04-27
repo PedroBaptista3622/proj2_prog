0027fa8b61b79c99a2e039a4b8da5f7e78dd92ee
João N. Matos <joaonmatos@gmail.com>
2018-04-27 18:32:20 +0100

    Continued work on Board class

--- proj2_prog/board.cpp
+++ proj2_prog/board.cpp
@@ -1,1 +1,18 @@
 #include "board.h"
+using namespace std;
+
+bool operator<(string s1, string s2)
+{
+	bool smaller = (s1.size() < s2.size());
+	int maxcompare;
+	if (smaller)
+		maxcompare = s1.size();
+	else
+		maxcompare = s2.size();
+	for (int i = 0; i < maxcompare; i++)
+	{
+		if (s1.at(i) != s2.at(i))
+			return s1.at(i) < s2.at(i);
+	}
+	return smaller;
+}
--- proj2_prog/board.h
+++ proj2_prog/board.h
@@ -6,8 +6,20 @@
 #include <map>
 #include <utility>
 #include "dictionary.h"
+using namespace std;
 
 //code
+
+/**
+ * Overloads the "less than" two strings, returning whether the first string 
+ * should be ordered before the second string
+ * 
+ * @param	s1	the first string to compare
+ * @param	s2	the second string to compare
+ * @return		the truth value of the predicate
+ */
+bool operator<(string s1, string s2);
+
 
 /**
  * Board is a class that stores the atributes of a crosswords puzzle, including
@@ -19,7 +31,7 @@
  * @author	João N. Matos
  * @author	Pedro Baptista
  * @version 0.1
- * @see	Dictionary
+ * @see		Dictionary
  */
 class Board
 {
@@ -33,31 +45,80 @@ class Board
 		 * Requires that a Dictionary is later added using the linkDictionary
 		 * procedure.
 		 * 
-		 * @param	lines	the number of lines the board will have
-		 * @param	columns	the number of columns in the board will have
+		 * @param	lines	The number of lines the board will have
+		 * @param	columns	The number of columns in the board will have
 		 */
 		Board(unsigned int lines, unsigned int columns);
 
 		/**
 		 * Constructs a Board object and associates a Dictionary to it.
 		 * 
-		 * @param	lines	the number of lines the board will have
-		 * @param	columns	the number of columns in the board will have
-		 * @param	*dictionary	pointer to the dictionary 
+		 * @param	lines		The number of lines the board will have
+		 * @param	columns		The number of columns in the board will have
+		 * @param	*dictionary	Pointer to the Dictionary object
 		 */
 		Board(unsigned int lines, unsigned int columns, Dictionary* dictionary);
+
+		/**
+		 * Constructs a Board object by loading an unfinished board from a file.
+		 * The dictionary pointer is initialised as NULL.
+		 * <p>
+		 * Requires a Dictionary to be later associated.
+		 * 
+		 * @param	filename	Name of the file to import
+		 */
+		Board(string filename);
+
+		/**
+		 * Constructs a Board object by loading an unfinished board from a file
+		 * and associates a Dictionary object to it.
+		 * 
+		 * @param	filename	Name of the file to import
+		 * @param	*dictionary	Pointer to the Dictionary object
+		 */
+		Board(string filename, Dictionary* dictionary);
 
 		//Methods
 
 		/**
 		 * Associates a Dictionary object by its pointer to the Board object.
 		 * 
-		 * @param	*dictionary	pointer to the dictionary
+		 * @param	*dictionary	Pointer to the Dictionary object
 		 */
 		void linkDictionary(Dictionary* dictionary);
 
-		//TODO
-		//TODO int insertWord(string position, string word);
+		/**
+		 * Tries to insert a word from the dictionary into the board, checking
+		 * for any ilegal overlap, inexistent words, repeated words, and lack of
+		 * space. Then updates the black space list.
+		 * <p>
+		 * Returns the following exit codes:
+		 * <p>
+		 *  0: success;
+		 * <p>
+		 * -1: inexistent word;
+		 * <p>
+		 * -2: ilegal overlap (different characters, black spaces);
+		 * <p>
+		 * -3: lack of space;
+		 * <p>
+		 * -4: repeated words.
+		 * 
+		 * @param	position	The position string to insert the word in
+		 * @param	word		The word to insert
+		 * @return				The exit code of the procedure
+		 */
+		int insertWord(string position, string word);
+
+		/**
+		 * Saves the board to a file, returning 0 if sucessful and -1 if not.
+		 * 
+		 * @param	filename	Name of the file to store the board in
+		 * @param	mode		Determines whether to save board as finished
+		 * @returns				Exit code
+		 */
+		int save(string filename, unsigned int mode);
+		
 	private:
 		//keeps size
 		struct size {
@@ -66,4 +127,28 @@ class Board
 		} number;
 
 		//keeps a list of words and its position
-		
+		map<string,string> words;
+
+		//keeps a list of the position of the black spaces
+		set<string> blackSpaces;
+
+		//keeps a map of the occupied spaces and respective characters
+		map<string, char> chars;
+
+		/**
+		 * Returns a string "a" to "zz" correspoding to a number up to 26^2 - 1.
+		 * 
+		 * @param	number	In range [0, 26^2 - 1]
+		 * @return			String from "a" to "zz"
+		 */
+		string convertPosNumber(unsigned int number);
+
+		/**
+		 * Returns a number up to 26^2 - 1 corresponding to a string
+		 * from "a" to "zz".
+		 * 
+		 * @param	string	From "a" to "zz"
+		 * @return			Number in range [0, 26^2 - 1]
+		 */
+		unsigned int convertPosString(string string);
+};
