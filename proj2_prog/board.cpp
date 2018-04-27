b1dab2e2c376a7005989b0c7ceeba1cae90f8d61
João N. Matos <joaonmatos@gmail.com>
2018-04-27 18:32:20 +0100

    Updated header Implemented easier constructors and dictionary linker

--- proj2_prog/board.cpp
+++ proj2_prog/board.cpp
@@ -1,1 +1,86 @@
 #include "board.h"
+using namespace std;
+
+
+/**
+ * Overloads the "less than" two strings, returning whether the first string 
+ * should be ordered before the second string
+ * 
+ * @param	s1	the first string to compare
+ * @param	s2	the second string to compare
+ * @return		the truth value of the predicate
+ */
+bool operator<(const string& lhs, const string& rhs)
+{
+	bool smaller = (lhs.size() < rhs.size());
+	int maxcompare;
+	if (smaller)
+		maxcompare = lhs.size();
+	else
+		maxcompare = rhs.size();
+	for (int i = 0; i < maxcompare; i++)
+	{
+		if (lhs.at(i) != rhs.at(i))
+			return lhs.at(i) < rhs.at(i);
+	}
+	return smaller;
+}
+
+/**
+ * Constructs a Board object without associating any Dictionary to it.
+ * The dictionary pointer is initialised as NULL.
+ * <p>
+ * Requires that a Dictionary is later added using the linkDictionary
+ * procedure.
+ * 
+ * @param	lines	The number of lines the board will have
+ * @param	columns	The number of columns in the board will have
+ */
+Board::Board(unsigned int lines, unsigned int columns)
+{
+	number.lines = lines;
+	number.columns = columns;
+	dictionary = NULL;
+}
+
+/**
+ * Constructs a Board object and associates a Dictionary to it.
+ * 
+ * @param	lines		The number of lines the board will have
+ * @param	columns		The number of columns in the board will have
+ * @param	dictionary	Pointer to the Dictionary object
+ */
+Board::Board(unsigned int lines, unsigned int columns, Dictionary* dictionary)
+{
+	number.lines = lines;
+	number.columns = columns;
+	this->dictionary = dictionary;
+}
+
+/**
+ * Associates a Dictionary object by its pointer to the Board object.
+ * 
+ * @param	dictionary	Pointer to the Dictionary object
+ */
+void Board::linkDic(Dictionary* dictionary)
+{
+	this->dictionary = dictionary;
+}
+
+/** TODO
+ * Compares two position strings and returns whether the first is before
+ * the second, using first the line and then the column as criteria.
+ * 
+ * @param	pos1	First position string
+ * @param	pos2	Second position string
+ * @return			Truth value of predicate
+ */
+/*
+ bool prevPos(string pos1, string pos2)
+{
+	struct coords {
+		string line;
+		string column;
+	} coords1, coords2;
+}
+*/
--- proj2_prog/board.h
+++ proj2_prog/board.h
@@ -6,8 +6,20 @@
 #include <map>
 #include <utility>
 #include "dictionary.h"
+using namespace std;
 
 //code
+
+/**
+ * Overloads the "less than" two strings, returning whether the first string 
+ * should be ordered before the second string
+ * 
+ * @param	lhs	the first string to compare
+ * @param	rhs	the second string to compare
+ * @return		the truth value of the predicate
+ */
+bool operator<(const string& lhs, const string& rhs);
+
 
 /**
  * Board is a class that stores the atributes of a crosswords puzzle, including
@@ -19,7 +31,7 @@
  * @author	João N. Matos
  * @author	Pedro Baptista
  * @version 0.1
- * @see	Dictionary
+ * @see		Dictionary
  */
 class Board
 {
@@ -33,37 +45,134 @@ class Board
 		 * Requires that a Dictionary is later added using the linkDictionary
 		 * procedure.
 		 * 
-		 * @param	lines	the number of lines the board will have
-		 * @param	columns	the number of columns in the board will have
+		 * @param	lines	The number of lines the board will have
+		 * @param	columns	The number of columns in the board will have
 		 */
 		Board(unsigned int lines, unsigned int columns);
 
 		/**
 		 * Constructs a Board object and associates a Dictionary to it.
 		 * 
-		 * @param	lines	the number of lines the board will have
-		 * @param	columns	the number of columns in the board will have
-		 * @param	*dictionary	pointer to the dictionary 
+		 * @param	lines		The number of lines the board will have
+		 * @param	columns		The number of columns in the board will have
+		 * @param	dictionary	Pointer to the Dictionary object
 		 */
 		Board(unsigned int lines, unsigned int columns, Dictionary* dictionary);
+
+		/**
+		 * Constructs a Board object by loading an unfinished board from a file.
+		 * The dictionary pointer is initialised as NULL.
+		 * <p>
+		 * Requires a Dictionary to be later associated.
+		 * 
+		 * @param	filename	Name of the file to import
+		 */
+		Board(string filename);
+
+		/**
+		 * Constructs a Board object by loading an unfinished board from a file
+		 * and associates a Dictionary object to it.
+		 * 
+		 * @param	filename	Name of the file to import
+		 * @param	dictionary	Pointer to the Dictionary object
+		 */
+		Board(string filename, Dictionary* dictionary);
 
 		//Methods
 
 		/**
 		 * Associates a Dictionary object by its pointer to the Board object.
 		 * 
-		 * @param	*dictionary	pointer to the dictionary
+		 * @param	dictionary	Pointer to the Dictionary object
 		 */
-		void linkDictionary(Dictionary* dictionary);
+		void linkDic(Dictionary* dictionary);
 
-		//TODO
-		//TODO int insertWord(string position, string word);
+		/**
+		 * Tries to insert a word from the dictionary into the board, checking
+		 * for any ilegal overlap, inexistent words, repeated words, and lack of
+		 * space. Then updates the black space list.
+		 * <p>
+		 * Returns the following exit codes:
+		 * <p>
+		 *  0: success;
+		 * <p>
+		 * -1: inexistent word;
+		 * <p>
+		 * -2: ilegal overlap (different characters, black spaces);
+		 * <p>
+		 * -3: lack of space;
+		 * <p>
+		 * -4: repeated words.
+		 * 
+		 * @param	position	The position string to insert the word in
+		 * @param	word		The word to insert
+		 * @return				The exit code of the procedure
+		 */
+		int insWord(string pos, string word);
+
+		/**
+		 * Saves the board to a file, returning 0 if sucessful and -1 if not.
+		 * 
+		 * @param	filename	Name of the file to store the board in
+		 * @param	mode		Determines whether to save board as finished
+		 * @returns				Exit code
+		 */
+		int save(string filename, unsigned int mode);
+
+		/**
+		 * Shows the board on screen.
+		 */
+		void show();
+		
 	private:
+
+		/**
+		 * Compares two position strings and returns whether the first is before
+		 * the second, using first the line and then the column as criteria.
+		 * 
+		 * @param	pos1	First position string
+		 * @param	pos2	Second position string
+		 * @return			Truth value of predicate
+		 */
+		bool prevPos(string pos1, string pos2);
+		
 		//keeps size
 		struct size {
-			unsigned int rows;
+			unsigned int lines;
 			unsigned int columns;
 		} number;
 
 		//keeps a list of words and its position
-		
+		map<string,string> words;
+
+		//keeps a list of the position of the black spaces
+		set<string, > blackSpaces;
+
+		//keeps a map of the occupied spaces and respective characters
+		map<string, char> chars;
+
+		//pointer to the dictionary
+		Dictionary* dictionary;
+
+		/**
+		 * Returns a string "a" to "zz" correspoding to a number up to 26^2 - 1.
+		 * 
+		 * @param	number	In range [0, 26^2 - 1]
+		 * @return			String from "a" to "zz"
+		 */
+		string cvtPosNr(unsigned int number);
+
+		/**
+		 * Returns a number up to 26^2 - 1 corresponding to a string
+		 * from "a" to "zz".
+		 * 
+		 * @param	string	From "a" to "zz"
+		 * @return			Number in range [0, 26^2 - 1]
+		 */
+		unsigned int cvtPosStr(string string);
+
+		/**
+		 * Fills all empty spaces with black spaces
+		 */
+		void blackout();
+};
