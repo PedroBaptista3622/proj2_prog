d96d827da59ff9c24a9c40f84a32c724e03b58cc
João N. Matos <joaonmatos@gmail.com>
2018-05-03 10:50:08 +0100

    got windows colour screen library

---
+++ colour_windows.cpp
@@ -0,0 +1,109 @@
+// PROG - MIEIC
+// JAS
+// Example of a programme that prints coloured characters on the console (in text mode)
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+//=================================================================================================
+//COLOUR CODES: (alternative: use symbolic constants)
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
+
+//=================================================================================================
+
+void clrscr(void)
+{
+	COORD coordScreen = { 0, 0 }; //upper left corner
+	DWORD cCharsWritten;
+	DWORD dwConSize;
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+	GetConsoleScreenBufferInfo(hCon, &csbi);
+	dwCnSize = csbi.dwSize.X * csbi.dwSize.Y;
+
+	//fill with spaces
+	FillConsoleOutputCharacter(hCon, TEXT(' '), dwConSize, coordScreen, &cCharsWritten);
+
+	//cursor to upper left corner
+	SetConsoleCursorPosition(hCon, coordScreen);
+}
+
+//=================================================================================================
+
+// Position the cursor at column 'x', line 'y'
+// The coordinates of upper left corner of the screen are (x,y)=(0,0)
+
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+//=================================================================================================
+
+// Set text colour
+
+void setcolor(unsigned int color)
+{
+	HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hcon, color);
+}
+
+//=================================================================================================
+
+// Set text colour and background
+
+void setcolor(unsigned int color, unsigned int background_color)
+{
+	HANDLE = hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (background_color == BLACK)
+		SetConsoleTextAttribute(hCon, color);
+	else
+		SetConsoleTextAttribute(hCon, color | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
+}
+
+//=================================================================================================
+
+// Fill the screen with coloured numbers
+
+int main()
+{
+	clrscr();
+
+	srand((unsigned int)time(NULL));
+
+	for (int x = 0; x < 80; x++)
+		for (int y = 0; y < 24; y++)
+		{
+			gotoxy(x, y);
+			if (rand() % 2 == 0)
+				setcolor(x % 15 + 1);
+			else
+				setcolor(y % 15 + 1, rand() % 2);
+			cout << x % 10;
+		}
+}
+
+//End of file
---
+++ colour_windows.h
@@ -0,0 +1,50 @@
+// PROG - MIEIC
+// JAS (Header by João N. Matos)
+// Header for a library that prints coloured characters on the console (in text mode)
+#ifndef COLOUR_WINDOWS_H
+#define COLOUR_WINDOWS_H
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+//=================================================================================================
+//COLOUR CODES: (alternative: use symbolic constants)
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHTGRAY 7
+#define DARKGRAY 8
+#define LIGHTBLUE 9
+#define LIGHTGREEN 10
+#define LIGHTCYAN 11
+#define LIGHTRED 12
+#define LIGHTMAGENTA 13
+#define YELLOW 14
+#define WHITE 15
+
+//=================================================================================================
+
+// Functions
+
+//Clears the screen
+void clrscr(void);
+
+// Position the cursor at column 'x', line 'y'
+// The coordinates of upper left corner of the screen are (x,y)=(0,0)
+void gotoxy(int x, int y);
+
+// Set text colour
+void setcolor(unsigned int color);
+
+// Set text colour and background
+void setcolor(unsigned int color, unsigned int background_color);
+
+#endif
+//End of file
