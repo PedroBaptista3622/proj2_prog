bfd50aafe22505da941cad60806790e5eb998bc8
Pedro Baptista <ze.pedro3622@outlook.pt>
2018-05-04 09:31:08 +0100

    Fixed all bugs of the dictionary class. Working.

--- proj2_prog/Dictionary.h
+++ proj2_prog/Dictionary.h
@@ -15,8 +15,9 @@ class Dictionary
 public:
 
 	Dictionary();
-	void delSpaces();							// Deletes empty spaces in words from vector
-	void printDictionary();						//Prints dictionary: [key]: [element1 of vector], [element2 of vector], [element3 of vector]
+	void printDictionary();//Prints dictionary: [key]: [element1 of vector], [element2 of vector], [element3 of vector]
+	void removeSpaces(string &word); //Removes space at index 0 of a word if it exists
+
 
 private:
 	map <string, vector <string> > dictionary; // Contains Every Word And Synonyms. The string element is the key.
