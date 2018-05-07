834def3d8ec6e819477c52907652bfb76fb3c010
João N. Matos <joaonmatos@gmail.com>
2018-05-07 22:24:02 +0100

    Disabled interface for Dictionary class constructor

--- proj2_prog/Dictionary.cpp
+++ proj2_prog/Dictionary.cpp
@@ -16,17 +16,19 @@ void Dictionary::removeSpaces(string &word)
 		}
 }
 
-Dictionary::Dictionary()
+Dictionary::Dictionary(string filename)
 {
+	/*
 	bool fileOpened = false;
 	string currentLine;
 	cout << "Please insert dictionary name" << endl;
 	cin >> name;
-
-
+	*/
+	this->name = filename;
 	ifstream dict;
 	dict.open(name);
 
+	/*
 	if (!dict.fail())
 	{
 		fileOpened = true;
@@ -37,54 +39,57 @@ Dictionary::Dictionary()
 		cerr << "Error opening file" << endl;
 	}
 
-	if (fileOpened)
+	*/
+ 	//if (fileOpened)
+	//{
+	while (getline(dict, currentLine))
 	{
-		while (getline(dict, currentLine))
+		string keyWord;
+		string currentWord; //1st synonym in the line
+		vector <string> synonyms;//Contains every synonym of each keyword
+		bool end = false;
+		int c = currentLine.find(':'); //c Contains the index of ':'
+
+		keyWord = stringToUpper(currentLine.substr(0, c));
+		currentLine = currentLine.substr(c + 2);
+
+		do
 		{
-			string keyWord;
-			string currentWord; //1st synonym in the line
-			vector <string> synonyms;//Contains every synonym of each keyword
-			bool end = false;
-			int c = currentLine.find(':'); //c Contains the index of ':'
+			c = currentLine.find(',');
 
-			keyWord = stringToUpper(currentLine.substr(0, c));
-			currentLine = currentLine.substr(c + 2);
-
-			do
+			if (c == string::npos)
 			{
-				c = currentLine.find(',');
+				c = currentLine.find((char)13);
+				end = true;
+				//c Contains de index if ',' if it exists, otherwise, contains de index of CR
+			}
 
-				if (c == string::npos)
-				{
-					c = currentLine.find((char)13);
-					end = true;
-					//c Contains de index if ',' if it exists, otherwise, contains de index of CR
-				}
-
-				currentWord = currentLine.substr(0, c);
-				removeSpaces(currentWord);//Removes the empty space at the start of the word
-				synonyms.push_back(stringToUpper(currentWord));//Adds word to the synonyms vector
+			currentWord = currentLine.substr(0, c);
+			removeSpaces(currentWord);//Removes the empty space at the start of the word
+			synonyms.push_back(stringToUpper(currentWord));//Adds word to the synonyms vector
 
 
-				if (!end) //If it's the last element this won't be executed
-				{
-					currentLine = currentLine.substr(c + 1);
-				}
+			if (!end) //If it's the last element this won't be executed
+			{
+				currentLine = currentLine.substr(c + 1);
+			}
 
 
 
-			} while (!end);
+		} while (!end);
 
-			this->dictionary[keyWord] = synonyms; //Adds to the dictionary map the first and second elements 
+		this->dictionary[keyWord] = synonyms; //Adds to the dictionary map the first and second elements 
 
-		}
+	}
 
-		dict.close(); //Closes file
+	dict.close(); //Closes file
+	/*
 	}
 	else
 	{
 		cerr << "Error opening file" << endl;
 	}
+	*/
 
 }
 
--- proj2_prog/Dictionary.h
+++ proj2_prog/Dictionary.h
@@ -14,9 +14,9 @@ class Dictionary
 {
 public:
 
-	Dictionary();
+	Dictionary(string filename);
+	
 	void printDictionary();//Prints dictionary: [key]: [element1 of vector], [element2 of vector], [element3 of vector]
-	void removeSpaces(string &word); //Removes space at index 0 of a word if it exists
 	string dictName();//Returns dictionary source file name
 	bool checkWordDictionary(const string &keyWord);//Checks if a certain word is present in the dictionary map container
 	vector <string> getSynonyms(const string &keyWord);//Returns the corresponding string vector of the keyWord
@@ -24,4 +24,7 @@ public:
 private:
 	map <string, vector <string> > dictionary; // Contains Every Word And Synonyms. The string element is the key.
 	string name;
+
+	
+	void removeSpaces(string &word); //Removes space at index 0 of a word if it exists
 };
