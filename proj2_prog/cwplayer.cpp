c7722bb7141c70cb129b1cbdeec944a86f47bb84
João N. Matos <joaonmatos@gmail.com>
2018-05-19 23:57:29 +0100

    Preliminary check to cwplayer

--- proj2_prog/cwplayer.cpp
+++ proj2_prog/cwplayer.cpp
@@ -22,32 +22,31 @@ int guessWord(Puzzle *puzzle, string position, string word)
 {
 	switch (puzzle->insGuess(position, word))
 	{
-	case -1:
-		cerr << "Word doesn't exist" << endl;
+		case -1:
+		cerr << "That word is too big" << endl;
 		return -1;
-		break;
 
-	case -2:
-		cerr << "Ilegal Overlap" << endl;
+		case -2:
+		cerr << "There was an ilegal overlap" << endl;
 		return -2;
-		break;
 
-	case -3:
-		cerr << "Lack of Space" << endl;
+		case -3:
+		cerr << "That word is too small" << endl;
 		return -3;
-		break;
 
-	case -4:
-		cerr << "Repeated Words" << endl;
+		case -4:
+		cerr << "You already input that word" << endl;
 		return -4;
-		break;
 
-	case -5:
+		case -5:
 		cerr << "Invalid Position" << endl;
 		return -5;
-		break;
 
-	case 0:
+		case -6:
+		cerr << "There is already a word in that position" << endl;
+		return -6;
+
+		case 0:
 		return 0;
 		break;
 	}
@@ -60,32 +59,35 @@ int main()
 	bool validBoardNumber = false;
 	ostringstream boardFileName;
 	bool errorOpeningFile = false;
-	
+
 
 	printLogo();
 
-		cout << "Please enter the number of the board you want to load (0 to 999)" << endl;
+	cout << "Please enter the number of the board you want to load (0 to 999)" << endl;
+	do
+	{
 
-		do
-		{
-			cin >> numberBoardToLoad;
+		numberBoardToLoad = readInt("Please enter a number from 0 to 999.\nYour input: ");
 
-			if (numberBoardToLoad >= 0 && numberBoardToLoad <= 999)
-				validBoardNumber = true;
+		if (numberBoardToLoad >= 0 && numberBoardToLoad <= 999)
+		validBoardNumber = true;
+		else
+		cout << "Please enter a number from 0 to 999.\nYour input: ";
 
-		} while (!validBoardNumber);
+	} while (!validBoardNumber);
 
-		boardFileName << "B";
-		boardFileName << fixed << setfill('0');
-		boardFileName << setw(3) << numberBoardToLoad;
-		boardFileName << ".txt";
+	boardFileName << "B";
+	boardFileName << fixed << setfill('0');
+	boardFileName << setw(3) << numberBoardToLoad;
+	boardFileName << ".txt";
 
-		Puzzle puzzle(boardFileName.str(), errorOpeningFile);
+	Puzzle puzzle(boardFileName.str(), errorOpeningFile);
 
-		if (errorOpeningFile)
-		{
-			cerr << "Error Opening Files, try again" << endl;
-		}
+	if (errorOpeningFile)
+	{
+		cerr << "Error loading board and dictionary." << endl;
+		cerr << "You might have input the wrong number, or the dictionary does not exist anymore" << endl;
+	}
 
 
 	string playerName;
@@ -94,22 +96,19 @@ int main()
 	char answer;
 
 	cout << "Time to get information about you, what's your name?" << endl;
-	
+
 	do
 	{
-		getline(cin, playerName);
-
-		cin.clear();
-		cin.ignore(999999);
+		playerName = readString("Please enter your name.\n");
 
 		cout << "Is \"" << playerName << "\" your name? (Y/N)" << endl;
-		
+
 		do
 		{
-			cin >> answer;
+			answer = readChar("Please answer the question (Y/N).\nYour answer: ");
 
 			if (answer == 'Y' || answer == 'y')
-			{	
+			{
 				confirmation = true;
 				validAnswer = true;
 			}
@@ -120,7 +119,7 @@ int main()
 			}
 			else
 			{
-				cout << "Invalid Answer, try again" << endl;
+				cout << "Please answer the question (Y/N).\nYour answer: " << endl;
 			}
 
 		} while (!validAnswer);
@@ -136,7 +135,7 @@ int main()
 	bool correctPuzzle = false;
 
 	player.start();
-	
+
 	do
 	{
 		do
@@ -148,15 +147,15 @@ int main()
 			cout << "1 - Guess a word" << endl;
 			cout << "2 - Delete word from board" << endl;
 			cout << "0 - Exit" << endl;
-			cout << "Note: If you exit before completing the puzzle correctly the progress will be lost" << endl;
+			cout << "Note: If you exit before completing the puzzle correctly your progress will be lost" << endl;
 
 			do
 			{
-				cin >> action;
+				action = readInt("Please enter a valid action.\nYour action: ");
 
 				if (action < 0 || action > 2)
 				{
-					cerr << "Invalid action, try again" << endl;
+					cout << "Please enter a valid action.\nYour action: ";
 				}
 
 			} while (action < 0 || action > 2);
@@ -164,44 +163,56 @@ int main()
 
 			if (action == 1)
 			{
-				cout << "Enter the position you want to guess (LcD)" << endl;
-				cin >> position;
-
+				wordPosition pos;
 				do
 				{
-					cout << "Enter the guess word" << endl;
-					cout << "You can also get some help by entering \'?\'" << endl;
-					cin >> guess;
+					cout << "Enter the position you want to guess (LcD)" << endl;
+					position = readString("Please enter a position (LcD)\n");
+					pos = wordPosition(position);
 
-
-					if (guess == "?")
+					if (!pos.inBoard(puzzle.getLines(),puzzle.getColumns()))
 					{
-						cout << "Hint: " << puzzle.anotherHint(position) << endl;
-						player.gotHint();
+						cout << "Invalid input. You must enter a valid position inside of the board." << endl;
 					}
-					else
+					do
 					{
-						if (guessWord(&puzzle, position, guess) == 0)
-							validGuess = true;
-
-						if (!validGuess)
-							cout << "Try another word" << endl;
-					}
+						cout << "Enter your guess" << endl;
+						cout << "You can also get some help by entering \'?\'" << endl;
+						guess = readString("Enter a word to guess or \"?\"\n");
 
 
-				} while (!validGuess);
+						if (guess == "?")
+						{
+							cout << "Hint: " << puzzle.anotherHint(position) << endl;
+							player.gotHint();
+						}
+						else
+						{
+							if (guessWord(&puzzle, position, guess) == 0)
+								validGuess = true;
+						}
+					} while (!validGuess);
+				} while (!pos.isValid());
 
 			}
 			else if (action == 2)
 			{
 				string position;
+				wordPosition pos;
 				bool validPosition = false;
 
 				cout << "Enter the position of the word you want to remove" << endl;
 
 				do
 				{
-					cin >> position;
+					position = readString("Enter the position of the word you want to remove\n");
+					pos = wordPosition(position);
+
+					if (!pos.inBoard(puzzle.getLines(),puzzle.getColumns()))
+					{
+						cout << "Invalid position. Please enter a valid position in the board.\n";
+						continue;
+					}
 
 					if (puzzle.remGuess(position) == 0)
 						validPosition = true;
@@ -243,7 +254,7 @@ int main()
 						fileName << fixed << setfill('0');
 						fileName << setw(3) << numberBoardToLoad;
 						fileName << "_p.txt";
-						
+
 						outFile << "The player \"" << player.getName() << "\" completed this puzzle in " << player.timeToComplete() << " seconds, and used " << player.getNumHints() << " hints" << endl;
 
 						cout << "You have completed this puzzle in " << player.timeToComplete() << " seconds, and used " << player.getNumHints() << " hints" <<  endl;
@@ -263,10 +274,10 @@ int main()
 				}
 
 				if (!validAnswer)
-					cerr << "Invalid answer, try again" << endl;
+				cerr << "Invalid answer, try again" << endl;
 
 			} while (!validAnswer);
-	
+
 		}
 
 		if (action == 0)
